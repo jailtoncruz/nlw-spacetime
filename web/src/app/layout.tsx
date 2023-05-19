da6df30aa1c8b3af8ba227c5581a5f68e79480b6
Jailton Cruz <42894082+jailtoncruz@users.noreply.github.com>
2023-05-19 14:43:59 -0300

    chore: add new features and configs

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,11 +1,21 @@
-import { Inter } from 'next/font/google'
+import {
+  Bai_Jamjuree as BaiJamjuree,
+  Roboto_Flex as Roboto,
+} from 'next/font/google'
+import React from 'react'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+const baiJamjuree = BaiJamjuree({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-bai-jamjuree',
+})
 
 export const metadata = {
-  title: 'Spacetime',
-  description: 'Save your timeline',
+  title: 'NLW Spacetime',
+  description:
+    'Uma cápsula do tempo construída com React, Next.js, TailwindCSS e Typescript',
 }
 
 export default function RootLayout({
@@ -15,7 +25,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
+      >
+        {children}
+      </body>
     </html>
   )
 }
