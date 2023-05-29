69ba22a984ff03d69e9899051063b6acb0c09021
Jailton Cruz <jailtoncruz01@gmail.com>
2023-05-28 22:22:06 -0300

    chore: remove logs and add new environmnet variable on web

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -1,13 +1,18 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server';
 
 const signInURL = `https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`
 
 export function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value
+
+  let redirectTo = request.url;
+  if (process.env.APP_URL)
+    redirectTo = request.url.replace("http://localhost", process.env.APP_URL)
+
   if (!token) {
     return NextResponse.redirect(signInURL, {
       headers: {
-        'Set-Cookie': `redirectTo=${request.url}; Path=/; HttpOnly; max-age=20`,
+        'Set-Cookie': `redirectTo=${redirectTo}; Path=/; HttpOnly; max-age=20`,
       },
     })
   }
